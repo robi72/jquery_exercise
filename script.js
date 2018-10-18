7d87334d65e010d78f76ccb2ea5044e6904aa090
Robert Grahovac <robert.grahovac@hotmail.com>
2018-10-18 11:39:19 +0200

    slide up+down

--- script.js
+++ script.js
@@ -3,24 +3,24 @@ $(document).ready(function() {
   $(".container-link2").hide();
   $(".container-link3").hide();
 
-  $("#link1").click(function() {
-    $(".container-link2").hide();
-    $(".container-link3").hide();
-    $(".container-link1").show();
+  $("#link1").css("color", "red").click(function() {
+    $(".container-link2").slideUp(2000);
+    $(".container-link3").slideUp(2000);
+    $(".container-link1").slideDown(2000);
   });
   
 
   $("#link2").click(function() {
-    $(".container-link1").hide();
-    $(".container-link3").hide();
-    $(".container-link2").show();
+    $(".container-link1").slideUp(2000);
+    $(".container-link3").slideUp(2000);
+    $(".container-link2").slideDown(2000);
   });
   
 
   $("#link3").click(function() {
-    $(".container-link1").hide();
-    $(".container-link2").hide();
-    $(".container-link3").show();
+    $(".container-link1").slideUp(2000);
+    $(".container-link2").slideUp(2000);
+    $(".container-link3").slideDown(2000);
   });
 
 });
